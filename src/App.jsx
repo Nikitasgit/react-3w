3ef1ad9dd8f6b34cd675be4dc16eb58b123354e3
Victor Leman <victorleman1@gmail.com>
2025-01-06 13:52:58 +0100

    commit correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import Form from "./components/Form";
-import UsersList from "./components/UsersList";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
   return (
     <>
-      <Form setUsers={setUsers} users={users} />
-      <UsersList users={users} />
+      <Form />
     </>
   );
 }
